c399bbfda7c53c8f8f845a423bc955a0cd0d004c
yaswanth chokkapu <yswanthchokkapu69@gmail.com>
2026-02-27 19:41:22 +0530

    newcommit

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,4 +1,4 @@
-const BASE_URL = "http://127.0.0.1:8000";
+const BASE_URL = "https://ai-sports-platform.onrender.com";
 
 export async function checkPerformance(data) {
   const res = await fetch(`${BASE_URL}/performance`, {
