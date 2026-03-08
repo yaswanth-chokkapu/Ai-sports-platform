e21f9dd0efaff2dca1e02223dcf6e419b04247d7
yaswanth chokkapu <yswanthchokkapu69@gmail.com>
2026-03-08 13:02:53 +0530

    main.py cors errror is modified

--- backend/app/main.py
+++ backend/app/main.py
@@ -11,7 +11,7 @@ app = FastAPI()
 # ✅ CORS MUST BE HERE (GLOBAL, BEFORE ROUTES)
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
