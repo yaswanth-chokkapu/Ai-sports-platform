f2d5d2d3a844da3a7fd00fd2780f3d694fb56938
yaswanth chokkapu <yswanthchokkapu69@gmail.com>
2026-03-08 14:48:45 +0530

    newcommit

--- frontend/src/pages/Performance.jsx
+++ frontend/src/pages/Performance.jsx
@@ -32,7 +32,7 @@ function Performance() {
      ========================= */
   async function checkPerformance() {
     try {
-      const response = await fetch("http://127.0.0.1:8000/session", {
+      const response = await fetch("https://ai-sports-platform.onrender.com/session", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({
